c8f3821796dc7484a21a822ba76e01a96de20978
adriyansyah.farhan <adriyansyah.farhan@solusi247.com>
2025-03-17 08:53:03 +0700

    add pagination support to list_ioc and list_log endpoints

--- migrations/create_table.py
+++ migrations/create_table.py
@@ -9,6 +9,7 @@ from models.log import LogsModel
 
 async def main():
     async with engine.begin() as conn:
+        await conn.run_sync(meta.drop_all)
         await conn.run_sync(meta.create_all, tables=[
             AdminModel,
             GroupModel,
